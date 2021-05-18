94bdab1167bd6a8058a7e5ff7a6670a5930044d1
Jason Tian <kuohuitien@gmail.com>
2021-05-19 00:54:49 +0800

    TEST: added auto export mode

--- org/.dir-locals.el
+++ org/.dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((org-mode . ((org-hugo-base-dir . "..")))
+ ((org-mode . ((eval . (org-hugo-auto-export-mode)))))
  ("daily" . ((org-hugo-base-dir . nil))))
